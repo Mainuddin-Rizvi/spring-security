34ef1d544a8bfe0b6645ae12d5c350d1327fc8af
Mainuddin Rizvi <web97development@gmail.com>
2024-06-17 21:41:22 +0530

    Basic RestApi Without Security

---
+++ spring-security-basic/src/main/java/com/springsecurity/spring_security_basic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.springsecurity.spring_security_basic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public  String sayWelcome(){
+        return "Welcome in Spring Security";
+    }
+}

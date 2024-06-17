a6921757674d2693252ae29ad705818a916c11df
Mainuddin Rizvi <web97development@gmail.com>
2024-06-17 22:15:03 +0530

    Understanding on how multiple requests work with out credentials and Internal FLow of Spring Security

--- spring-security-basic/src/main/java/com/springsecurity/spring_security_basic/SpringSecurityBasicApplication.java
+++ spring-security-basic/src/main/java/com/springsecurity/spring_security_basic/SpringSecurityBasicApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.concurrent.Flow;
+
 @SpringBootApplication
 public class SpringSecurityBasicApplication {
 
@@ -12,3 +14,24 @@ public class SpringSecurityBasicApplication {
 	}
 
 }
+
+//After first request send Spring Security create JsessionId that's why we dont need to login again and again
+//Check via the postman use basic auth and put id and password in it send request than remove  jsession from cookies .
+
+
+
+//Flow
+//						Autherntication
+//							|
+//	UserEnteredDetails ----->AuthenticationFilter -------->AuthenticationManager ------> AuthenticationProvider
+//							|															|				|
+//						SecurityContext										UserDetailsService       PasswordEncoder
+
+
+//org.springframework.security.web.authentication.AuthenticationFilter (Class)
+//org.springframework.security.authentication.AuthenticationManager (Interface)
+//org.springframework.security.authentication.AuthenticationProvider (Interface)
+//org.springframework.security.core.userdetails.UserDetailsService (Interface)
+//org.springframework.security.crypto.password.PasswordEncoder (Interface)
+//org.springframework.security.core.context.SecurityContext (Interface)
+//org.springframework.security.core.Authentication (Interface)

8233ead8e08d92a7738af96ca7c4519244aa086e
Mainuddin Rizvi <web97development@gmail.com>
2024-07-14 19:41:43 +0530

    Add CustomUserdetailsService | Register | SqlScripts |Jdbc after InMemory

---
+++ 4-springsecurity/src/main/java/com/mainuddin/springsecurity/config/MoneyBankUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.mainuddin.springsecurity.config;
+
+import com.mainuddin.springsecurity.model.Customer;
+import com.mainuddin.springsecurity.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MoneyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
---
+++ 4-springsecurity/src/test/java/com/mainuddin/springsecurity/MoneyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainuddin.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

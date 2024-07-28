ff5165a1fadc9f816b031f02ad6ef07220210072
Mainuddin Rizvi <web97development@gmail.com>
2024-07-28 23:13:33 +0530

    Method Level Security Post and Pre

--- 9-springsecurity/src/main/java/com/mainuddin/springsecurity/MoneyBankBackendApplication.java
+++ 9-springsecurity/src/main/java/com/mainuddin/springsecurity/MoneyBankBackendApplication.java
@@ -2,10 +2,12 @@ package com.mainuddin.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true)
 public class MoneyBankBackendApplication {
 
 	public static void main(String[] args) {
--- 9-springsecurity/src/main/java/com/mainuddin/springsecurity/repository/LoanRepository.java
+++ 9-springsecurity/src/main/java/com/mainuddin/springsecurity/repository/LoanRepository.java
@@ -1,13 +1,13 @@
 package com.mainuddin.springsecurity.repository;
 import java.util.List;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import com.mainuddin.springsecurity.model.Loans;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
-
+    // @PreAuthorize("hasRole('USER')")
     List<Loans> findByCustomerIdOrderByStartDtDesc(long customerId);
 
 }
